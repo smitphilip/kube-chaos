fb862fb0ddcdaa60c0ed5ffbd6b2c3d8f6a00ede
Philip Smit <philipsmit@live.co.za>
2020-10-08 22:13:30 +0200

    templates added, features added

--- kubechaos.sh
+++ kubechaos.sh
@@ -8,11 +8,11 @@ LOGFILE=/var/log/kubechaos.log
 DATETIME=$(date '+%d-%m-%Y %H:%M:%S')
 # === === === === === #
 
-#kubectl get ns $CHAOS_SPACE 2>/dev/null
-#if [ $? -ne 0 ]; then
-#  echo "Failed. Namespace $CHAOS_SPACE not found"
-#  exit 7
-#fi
+kubectl get ns $CHAOS_SPACE &>/dev/null
+if [ $? -ne 0 ]; then
+  echo "Failed. Namespace $CHAOS_SPACE not found"
+  exit 7
+fi
 
 function deleteAll {
     # delete all objects in namespace
@@ -48,6 +48,7 @@ function addNetpol {
     TMPFILE=.tmp_netpol.yml
     sed "s/\^namespace\^/$CHAOS_SPACE/g" templates/template-netpol.yml > $TMPFILE
 
+    kubectl apply -f $TMPFILE
     if [ $? -eq 0 ]; then
       echo "$DATETIME - NETPOL - Config Applied" >> $LOGFILE
     fi
@@ -70,3 +71,4 @@ RNDFUNC=$[$RANDOM % ${#FUNCTIONS[@]}]
 
 # call random function
 ${FUNCTIONS[$RNDFUNC]}
+
